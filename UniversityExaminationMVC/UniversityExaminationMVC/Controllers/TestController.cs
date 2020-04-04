6b192bebf2f65749700b8150a91c31c75ad6b5c9
Aatish chaudhari <40951689+Aatish13@users.noreply.github.com>
2020-04-04 22:45:25 +0530

    Timer added

--- UniversityExaminationMVC/UniversityExaminationMVC/Controllers/TestController.cs
+++ UniversityExaminationMVC/UniversityExaminationMVC/Controllers/TestController.cs
@@ -36,14 +36,17 @@ namespace UniversityExaminationMVC.Controllers
         public ActionResult UpcomingExam()
         {
             Authorize();
-            DateTime date = DateTime.Now.Date;
+            DateTime date = DateTime.Now;
             ViewBag.Date = date;
             List<Exam> exa = db.Exams.Where(x => true).ToList();
             List<Exam> exams = new List<Exam>();
             DateTime exdate;
             foreach (Exam x in exa)
             {
-                if (DateTime.Compare(DateTime.Now, x.date) < 0)
+
+                TimeSpan ts = DateTime.Now - x.date;
+                ViewBag.log = ts.TotalMinutes;
+                if (ts.TotalMinutes  <= 15)
                 {
                     exams.Add(x);
                 }
@@ -52,7 +55,10 @@ namespace UniversityExaminationMVC.Controllers
             List<Paper> papers = new List<Paper>();
             foreach (Paper pp in db.Papers.Where(x => true).ToList())
             {
-                if (DateTime.Compare(DateTime.Now, pp.paperDate) < 0)
+
+                TimeSpan ts = DateTime.Now - pp.paperDate;
+                ViewBag.log = ts.TotalMinutes;
+                if (ts.TotalMinutes <= 15)
                 {
                     papers.Add(pp);
                 }
@@ -96,22 +102,51 @@ namespace UniversityExaminationMVC.Controllers
         {
           
             Paper paper = db.Papers.Where(x => x.Id == id).SingleOrDefault();
-           
+            
             if (paper != null)
             {
-                Session["PaperId"] = id;
-                List<Question> qq = new List<Question>();
-                foreach (PaperQuestion pq in db.PaperQuestions.Where(x => x.PaperId == id).ToList())
+
+                int dif = DateTime.Compare(paper.paperDate,DateTime.Now);
+
+                TimeSpan ts =  DateTime.Now - paper.paperDate  ;
+                ViewBag.log = ts.TotalMinutes;
+                if (ts.TotalMinutes >= 0 && ts.TotalMinutes <=15)
                 {
-                    Question q = db.Questions.Where(x => x.QuestionId == pq.QuestionId).SingleOrDefault();
-                    if (q != null)
+
+                    Session["PaperId"] = id;
+                    List<Question> qq = new List<Question>();
+                    foreach (PaperQuestion pq in db.PaperQuestions.Where(x => x.PaperId == id).ToList())
                     {
-                        qq.Add(q);
+                        Question q = db.Questions.Where(x => x.QuestionId == pq.QuestionId).SingleOrDefault();
+                        if (q != null)
+                        {
+                            qq.Add(q);
+                        }
                     }
+                    DateTime timerDate = paper.paperDate.AddMinutes(paper.Duration);
+                    ViewBag.PaperDate = string.Format("{0:MMMM dd, yyyy HH:mm:ss}", timerDate);// paper.paperDate.Month +" "+paper.paperDate.Day+","+paper.paperDate.TimeOfDay ;
+                    ViewBag.Questions = qq;
+                    ViewBag.statusCode = 100;
+
                 }
-                ViewBag.Questions = qq;
+                else if(ts.TotalMinutes > 15)
+                {
+                    ViewBag.error = "You are late to attempt the paper ";
+                    ViewBag.statusCode = 101;
+                }
+                else  {
+                    ViewBag.error = "Papern has not started Time remaning ="+ts.TotalMinutes +" Minutes";
+                    ViewBag.statusCode =101;
+                }
+
+
             }
-          
+            else
+            {
+                ViewBag.error = "No paper found";
+                ViewBag.statusCode = 102;
+            }
+
             return View();
         }
    
